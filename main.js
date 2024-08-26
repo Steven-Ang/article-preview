0dbd8996997bdfe462d5a182c76e8d79eb283eb9
Steven Ang <spikegreen2@gmail.com>
2024-08-26 21:55:24 +0800

    Add mobile design

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Test");
+const footer = document.querySelector(".article-footer");
+const shareButton = document.querySelector(".share-icon");
+const leftFooterChild = footer.querySelector(".left");
+const authorContainer = leftFooterChild.querySelector(".author-container");
+const iconsContainer = leftFooterChild.querySelector(".icons-container");
+
+shareButton.addEventListener("click", () => {
+  if (iconsContainer.classList.contains("hidden")) {
+    footer.classList.add("hover-background");
+    authorContainer.classList.add("hidden");
+    iconsContainer.classList.remove("hidden");
+  } else {
+    footer.classList.remove("hover-background");
+    authorContainer.classList.remove("hidden");
+    iconsContainer.classList.add("hidden");
+  }
+});
