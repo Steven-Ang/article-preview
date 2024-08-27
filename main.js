7250257c57c5ec797da401386ee6a262d86b93a5
Steven Ang <spikegreen2@gmail.com>
2024-08-27 19:08:33 +0800

    Add desktop desgin

--- main.js
+++ main.js
@@ -1,17 +1,78 @@
 const footer = document.querySelector(".article-footer");
 const shareButton = document.querySelector(".share-icon");
-const leftFooterChild = footer.querySelector(".left");
-const authorContainer = leftFooterChild.querySelector(".author-container");
-const iconsContainer = leftFooterChild.querySelector(".icons-container");
+const authorContainer = footer.querySelector(".author-container");
+const iconsContainer = footer.querySelector(".icons-container");
 
-shareButton.addEventListener("click", () => {
-  if (iconsContainer.classList.contains("hidden")) {
-    footer.classList.add("hover-background");
-    authorContainer.classList.add("hidden");
-    iconsContainer.classList.remove("hidden");
-  } else {
-    footer.classList.remove("hover-background");
+window.addEventListener("resize", () => {
+  if (
+    window.innerWidth >= 1200 &&
+    iconsContainer.classList.contains("active")
+  ) {
+    shareButton.classList.remove("active");
+    iconsContainer.classList.remove("active-background");
+    iconsContainer.classList.remove("active");
+    iconsContainer.classList.add("hidden");
+
+    iconsContainer.style.left = "";
+    iconsContainer.style.top = "";
+  } else if (
+    window.innerWidth < 1200 &&
+    !iconsContainer.classList.contains("hidden")
+  ) {
+    shareButton.classList.remove("active");
+    footer.classList.remove("active-background");
     authorContainer.classList.remove("hidden");
     iconsContainer.classList.add("hidden");
+  }
+});
+
+shareButton.addEventListener("click", () => {
+  if (window.innerWidth >= 1200) {
+    if (iconsContainer.classList.contains("active")) {
+      shareButton.classList.remove("active");
+      iconsContainer.classList.remove("active-background");
+      iconsContainer.classList.remove("active");
+      iconsContainer.classList.add("hidden");
+
+      iconsContainer.style.left = "";
+      iconsContainer.style.top = "";
+    } else {
+      shareButton.classList.add("active");
+      iconsContainer.classList.add("active-background");
+      iconsContainer.classList.add("active");
+      iconsContainer.classList.remove("hidden");
+
+      const { top: shareButtonTop } = shareButton.getBoundingClientRect();
+      const {
+        offsetLeft: shareButtonOffsetLeft,
+        offsetWidth: shareButtonOffsetWidth,
+      } = shareButton;
+
+      const { offsetWidth: iconsContainerOffsetWidth } = iconsContainer;
+
+      const shareButtonCenter = (
+        shareButtonOffsetLeft +
+        shareButtonOffsetWidth / 2
+      ).toFixed();
+
+      const iconsContainerPosition =
+        shareButtonCenter - (iconsContainerOffsetWidth / 2).toFixed();
+      const iconsContainerTopPosition = (shareButtonTop - 60).toFixed();
+
+      iconsContainer.style.left = `${iconsContainerPosition}px`;
+      iconsContainer.style.top = `${iconsContainerTopPosition}px`;
+    }
+  } else {
+    if (iconsContainer.classList.contains("hidden")) {
+      shareButton.classList.add("active");
+      footer.classList.add("active-background");
+      authorContainer.classList.add("hidden");
+      iconsContainer.classList.remove("hidden");
+    } else {
+      shareButton.classList.remove("active");
+      footer.classList.remove("active-background");
+      authorContainer.classList.remove("hidden");
+      iconsContainer.classList.add("hidden");
+    }
   }
 });
